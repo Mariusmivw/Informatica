0a3cffc194ae00870ef09efbfed23fbe360298a6
Mariusmivw <mariusmivw@gmail.com>
2017-09-30 04:16:52 +0200

    added more words support and commented on a lot of things

--- Wordmind.py
+++ Wordmind.py
@@ -1,18 +1,28 @@
+import math
+import time
 import random
+import http
 import tkinter as tk
 from html.parser import HTMLParser
 
 words = []
 class MyHTMLParser(HTMLParser):
-    def handle_data(self, data):
+    def handle_data(self, data): # Redifining the handle_data function
         if ("".join(data.split()) != "" and "".join(data.split()).startswith("Letter") == False):
-            words.append(data)
+            words.append(data) # add the words to the word list
 
 hp = MyHTMLParser()
-with open("6Letters.html") as file:
-    hp.feed(file.read())
+
+with open("6Letters.html") as file: # getting the contents of '6Letters.html'
+    hp.feed(file.read())            # feeding it to the HTML-parser so it can do its thing
+
+with open("additionalWords.txt") as file: #adding words from the additionalWords list
+    addedWords = file.read().upper().split()
+    for i in range(len(addedWords)):
+        words.append(addedWords[i-1])
 
 secretword = words[random.randint(0,len(words)-1)].upper()
+# secretword = "zelden".upper()
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -20,6 +30,8 @@ class Application(tk.Frame):
         self.pack(anchor="nw")
         self.create_widgets(chars=len(secretword))
         self.stay = []
+        self.canvas.itemconfig(self.text[0], text=secretword[0], fill="#AFAFAF")
+        self.canvas.itemconfig(self.correct[0], state="normal")
 
     def create_widgets(self, chars=6, rows=5, boxsize=75, boxspacing=1):
         # self.hi_there = tk.Button(self)
@@ -34,13 +46,20 @@ class Application(tk.Frame):
         self.boxsize    = boxsize
         self.boxspacing = boxspacing
 
-        self.enter = tk.Button(self)
+        self.canvas = tk.Canvas(self, width=chars*(boxsize+boxspacing)-boxspacing, height=rows*(boxsize+boxspacing)-boxspacing)
+        self.canvas.pack(side="top") # ^^making a canvas with at the right size^^
+
+        self.enter = tk.Button(self) # making a guess button, if you don't like pressing Enter
         self.enter["text"] = "Make my guess!"
         self.enter["command"] = self.make_guess
-        self.enter.pack(side="bottom")
+        self.enter.pack(side="left")
 
-        self.canvas = tk.Canvas(self, width=chars*(boxsize+boxspacing)-boxspacing, height=rows*(boxsize+boxspacing)-boxspacing)
-        self.canvas.pack(side="top")
+        self.again = tk.Button(self) # making a restart button, if you don't feel like solving your currnt word
+        self.again["text"] = "Restart"
+        self.again["command"] = self.restart
+        self.again.pack(side="left")
+
+
 
         # self.text = tk.Label(self)
         # self.text.pack()
@@ -58,55 +77,77 @@ class Application(tk.Frame):
         for i in range(chars):
             if (line==0):
                 for u in range(rows):
+                    self.startTime = time.time() # setting the startTime so I can measure how long it took to guess the word
                     self.canvas.create_rectangle((boxsize+boxspacing)*i, (boxsize+boxspacing)*u, boxsize+(boxsize+boxspacing)*i, boxsize+(boxsize+boxspacing)*u, fill="#1768F5", outline="#0A3175")
             self.circle.append(self.canvas.create_oval((boxsize+boxspacing)*i, line*(boxsize+boxspacing), boxsize+(boxsize+boxspacing)*i, boxsize+line*(boxsize+boxspacing), fill="#EEEE11", outline="#DCCE52", state="hidden"))
             self.correct.append(self.canvas.create_rectangle((boxsize+boxspacing)*i, line*(boxsize+boxspacing), boxsize+(boxsize+boxspacing)*i, boxsize+line*(boxsize+boxspacing), fill="#FF0000", outline="#7C0000", state="hidden"))
             self.text.append(self.canvas.create_text(((boxsize+boxspacing)*i+boxsize/2, boxsize/2+line*(boxsize+boxspacing)), text="", font=("", boxsize-20), fill="#FFFFFF"))
+            # [81-84] >> creating the proper backgrounds for the text, and the text itself
 
         # self.quit = tk.Button(self, text="QUIT", fg="red",
         #                      command=root.destroy)
         # self.quit.pack(side="bottom")
 
-    def make_guess(self):
-        print("You guessed:", "".join(self.guess))
-        secretwordTemp = list(secretword)
+    def restart(self):
+        self.canvas.delete("all") # clearing the canvas
+        secretword = words[random.randint(0,len(words)-1)].upper() # selecting a new secret word
+        self.initialize_line()
+        self.stay = [0] # there are no chars that should stay yet, except the first letter
+        self.canvas.itemconfig(self.text[0], text=secretword[0], fill="#AFAFAF") # setting the first letter
+        self.canvas.itemconfig(self.correct[0], state="normal") # making the background red
+
+    def make_guess(self): # go to the next function first to know what this is about (line 128)
+        print("You guessed: " + "".join(self.guess)) # info on what the guessed word was in the console
+        secretwordTemp = list(secretword) # just the same, but this one can be alterd without consequences
+        guess = self.guess # so works this one
 
         for i in range(len(self.guess)):
-            if (self.guess[i] == secretword[i]):
-                self.canvas.itemconfig(self.correct[i], state="normal")
-                secretwordTemp[i] = "~"
-                self.stay.append(i)
-                if (self.guess == list(secretword)):
-                    correct = True
+            if (self.guess[i] == secretword[i]): # the letter is in the correct position
+                self.canvas.itemconfig(self.correct[i], state="normal") # making the background red
+                secretwordTemp[i] = "~" # making note that the this letter has been guessed, for when a letter is typed in multiple times (like in: 'tree')
+                self.stay.append(i) # this letter should stay next round
 
         for i in range(len(self.guess)):
-            if (self.guess[i] in secretwordTemp):
-                self.canvas.itemconfig(self.circle[i], state="normal")
-                secretwordTemp[secretwordTemp.index(self.guess[i])] = "~"
+            if (self.guess[i] in secretwordTemp): # the letter is somewhere in the word
+                self.canvas.itemconfig(self.circle[i], state="normal") # making the yellow circle appear (behind the red square)
+                secretwordTemp[secretwordTemp.index(self.guess[i])] = "~" # making note that the this letter has been guessed, for when a letter is typed in multiple times (like in: 'tree')
 
         self.initialize_line(chars=self.chars, rows=self.rows, boxsize=self.boxsize, boxspacing=self.boxspacing, line=self.lines)
-        for i in range(len(self.stay)):
+        for i in range(len(self.stay)): # making the letter that were correct show and have the red background in the next round
             self.canvas.itemconfig(self.text[self.stay[i]], text=secretword[self.stay[i]], fill="#AFAFAF")
             self.canvas.itemconfig(self.correct[self.stay[i]], state="normal")
 
+        if (guess == list(secretword)): # the word has been guessed
+            print("You found it!!!")
+            print("It took you " + str(math.floor((time.time() - self.startTime)*10)/10) + "s") # info to the console about how long it took to guess
+            self.canvas.itemconfig(self.text[-1], text="s")
+            for i in range(len(str(math.floor((time.time() - self.startTime)*10)/10))):
+                self.canvas.itemconfig(self.text[-i-2], text=list(str(math.floor((time.time() - self.startTime)*10)/10))[-i-1])
+            for i in range(5-len(str(math.floor((time.time() - self.startTime)*10)/10))):
+                self.canvas.itemconfig(self.text[i], text="")
+            # [123-127] >> making the time appear correctly in the GUI
+
     def key(self, event): # this runs whenever a key is pressed
         # print(event.keysym)
-        if (len(event.keysym) == 1):
+        if (len(event.keysym) == 1): # the key pressed was probably a letter (could be a number, but that's your own stupidity)
             self.guess.append(event.keysym.upper())
             self.canvas.itemconfig(self.text[len(self.guess)-1], text=self.guess[len(self.guess)-1], fill="#FFFFFF")
-            if (len(self.guess)-1 in self.stay and self.guess[len(self.guess)-1] != secretword[len(self.guess)-1]):
-                self.canvas.itemconfig(self.correct[len(self.guess)-1], state="hidden")
+            # ^^^^making the letter white, because it could have been gray from beeing right in a guess before and not yet typed in
+            if (len(self.guess)-1 in self.stay and self.guess[len(self.guess)-1] != secretword[len(self.guess)-1]): # the letter typed is not the displayed, (correct), letter
+                self.canvas.itemconfig(self.correct[len(self.guess)-1], state="hidden") # making the background not red anymore because it's now wrong
+
         elif (event.keysym == "BackSpace"):
-            self.guess = self.guess[:-1]
-            self.canvas.itemconfig(self.text[len(self.guess)], text="")
-            if (len(self.guess) in self.stay):
-                self.canvas.itemconfig(self.text[len(self.guess)], text=secretword[len(self.guess)], fill="#AFAFAF")
-                self.canvas.itemconfig(self.correct[len(self.guess)], state="normal")
+            self.guess = self.guess[:-1] # remove the last guessed letter from the list
+            self.canvas.itemconfig(self.text[len(self.guess)], text="") # removing that letter from the screen
+            if (len(self.guess) in self.stay): # you already know the letter that should be there
+                self.canvas.itemconfig(self.text[len(self.guess)], text=secretword[len(self.guess)], fill="#AFAFAF") # making the letter that should be there appear and gray
+                self.canvas.itemconfig(self.correct[len(self.guess)], state="normal") # making the background red again
+
         elif (event.keysym == "Return"):
-            if (len(self.guess) == len(secretword)):
-                self.make_guess()
+            if (len(self.guess) == len(secretword)): # the typed in word is the correct length
+                self.make_guess() # now you can go back to that function (line 99)
 
 root = tk.Tk()
 app = Application(master=root)
-app.bind_all("<KeyPress>", app.key)
+app.bind_all("<KeyPress>", app.key) # actually making the key function run every time a key is pressed
 app.mainloop()
